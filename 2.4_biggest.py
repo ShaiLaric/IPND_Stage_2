022c88136cccf686d15a348363d4774a88a2b489
Shai Laric <slaric@slaric.com>
2015-10-30 16:54:50 -0400

    Updated with Dave's solution.

--- 2.4_biggest.py
+++ 2.4_biggest.py
@@ -9,6 +9,20 @@ def biggest(a, b, c):
 	else:
 		return c
 
+# Redefined below with Dave's solution:
+
+def biggest(a, b, c):
+	if a > b:
+		if a > c:
+			return a
+		else:
+			return c
+	else:
+		if b > c:
+			return b
+		else:
+			return c
+
 print biggest(3, 6, 9)
 print biggest(9, 3, 6)
 print biggest(3, 3, 9)
