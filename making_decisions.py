f8e204a31cc38408c9b3de06adab2eb79cb5f140
Shai Laric <slaric@slaric.com>
2015-10-30 16:02:27 -0400

    Add making_decisions.py - middle of Lesson 2.4

---
+++ making_decisions.py
@@ -0,0 +1,47 @@
+# Lesson 2.4: Making Decisions - If Statements
+
+# We'll often write programs that need to make comparisons between values.
+# We can do comparisons just like we do in math with the < and > signs.
+# We can also do equality comparisons with != (not equal) and ==.
+# Comparisons always return a Boolean value (either True or False).
+
+# https://www.udacity.com/course/viewer#!/c-nd000/l-4196788670/e-48727556/m-48724313
+
+#print 2 < 3			# True
+#print 21 < 3		# False
+#print 7 * 3 < 21	# False
+#print 7 * 3 != 21	# False
+#print 7 * 3 == 21	# True
+
+# The above are comparison operators.
+
+#if 7 > 3:
+#	print "7 is bigger than 3."
+
+#def absolute(x):
+#	if x < 0:
+#		x = -x
+#	return x
+#
+#print absolute(0)
+
+def bigger(a, b):
+	if a > b:
+		r = a
+	else:
+		r = b
+	return r
+
+print bigger(2,7)
+print bigger(3,2)
+print bigger(3,3)
+
+def is_friend(name):
+	if name[0] == "D" or name[0] == "N":
+		return True
+	else:
+		return False
+
+print is_friend("Diane")
+print is_friend("Ned")
+print is_friend("Arry")
