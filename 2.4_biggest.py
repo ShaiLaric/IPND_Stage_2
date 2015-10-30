792655c1d6dbb648e076fcf9f9bb20c5affdf343
Shai Laric <slaric@slaric.com>
2015-10-30 16:37:03 -0400

    Add 2.4_biggest.py in mid-development

---
+++ 2.4_biggest.py
@@ -0,0 +1,20 @@
+# Lesson 2.4: Making Decisions - If Statements
+# Final Quiz
+
+def biggest(a, b, c):
+	if a >= b and a >= c:
+		return a
+	if b >= a and b >= c:
+		return b
+	else:
+		return c
+
+print biggest(3, 6, 9)
+print biggest(9, 3, 6)
+print biggest(3, 3, 9)
+print biggest(9, 3, 9)
+# mine next
+print biggest(1, 2, 3)
+print biggest(4, 2, 2)
+print biggest(6, 5, 6)
+print biggest(5, 6, 5)
